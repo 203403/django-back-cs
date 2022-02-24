f67b4d4836778f402798b1cb1c64cdaf2469f090
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-02-24 10:04:24 -0600

    change Login behavior authentication

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -59,8 +59,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
-
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 'rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
 
 MIDDLEWARE = [
