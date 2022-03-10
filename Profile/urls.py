9eac1c1312ad8d2e9428e24c4d9e394ac1804329
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-10 05:15:41 -0600

    Profile methods post, put, get, delete was added

--- Profile/urls.py
+++ Profile/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, re_path
 from django.conf.urls import include
 
 # Importacion de vistas
-from Profile.views import LoadProfile
-from Profile.views import LoadProfileDetail
+from Profile.views import LoadProfileImage
+from Profile.views import ProfileUserData
 
 urlpatterns = [
-    re_path(r'^user/$', LoadProfile.as_view()),
-    re_path(r'^user/(?P<pk>\d+)$', LoadProfileDetail.as_view()),
+    re_path(r'^user/$', LoadProfileImage.as_view()),
+    re_path(r'^user/(?P<pk>\d+)$', ProfileUserData.as_view()),
+    re_path(r'^user-img/(?P<pk>\d+)$', LoadProfileImage.as_view()),
+    # re_path(r'^user/(?P<pk>\d+)$', LoadProfileDetail.as_view()),
 
 ]
--- Profile/views.py
+++ Profile/views.py
@@ -18,32 +18,7 @@ import json
 # Create your views here.
 
 
-class LoadProfile(APIView):
-
-    def get_object(self, pk):
-        try:
-            return User.objects.get(id=pk)
-        except User.DoesNotExist:
-            return 0
-
-    def put(self, request):
-        if 'url_img' not in request.data:
-            raise exceptions.ParseError("Ningun archivo seleccionado")
-        file = request.data['url_img']
-        userId = request.data['user_id']
-        user = self.get_objectUser(userId)
-        if(user != 0):
-            serializer = ProfileSerializer(data=request.data)
-            if serializer.is_valid():
-                validated_data = serializer.validated_data
-                profile = Profile(**validated_data)
-                profile.save()
-                serializerResponse = ProfileSerializer(profile)
-                return Response(serializerResponse.data, status.HTTP_200_OK)
-        return Response("error", status = status.HTTP_400_BAD_REQUEST)
-
-
-class LoadProfileDetail(APIView):
+class LoadProfileImage(APIView):
 
     def get_object(self, pk):
         try:
@@ -51,33 +26,70 @@ class LoadProfileDetail(APIView):
         except ProfileTable.DoesNotExist:
             return 0
 
+    def post(self, request):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            profileInitial = Profile(**validated_data)
+            profileInitial.save()
+            serializerResponse = ProfileSerializer(profileInitial)
+            return Response("success", status = status.HTTP_200_OK)
+        return Response("Error: No es puede procesar la solicitud", status = status.HTTP_400_BAD_REQUEST)   
+
+    def put(self, request, pk, format=None):
+        file = request.data['url_img']
+        idResponse = self.get_object(pk)
+        if 'image' not in request.data:
+                raise exceptions.ParseError("Ningun archivo seleccionado") 
+        if(idResponse != 0):
+            serializer = ProfileSerializer(idResponse)
+            serializer.url_img = file
+            serializer.save()  
+        responseOk = self.responseCustom(serializer.data, "success", status.HTTP_200_OK)
+        return Response(responseOk)
+    
+    def delete(self, request, pk):
+        profileImg = self.get_object(pk)
+        if profileImg != 0:
+            profileImg.url_img.delete()
+            return Response("La imagen fue eliminada",status=status.HTTP_204_NO_CONTENT)
+        return Response("Error al buscar la imagen", status = status.HTTP_400_BAD_REQUEST)
+
+    def responseCustom(self, data, respuesta, status):
+        responseOk = {"messages": respuesta,
+                      "pay_load": data, "status": status}
+        responsOk = json.dumps(responseOk)
+        return responseOk
+
+class ProfileUserData(APIView):
+    
+    def get_object(self, pk):
+        try:
+            return ProfileTable.objects.get(pk=pk)
+        except ProfileTable.DoesNotExist:
+            return 0
+    
     def get(self, request, pk, format=None):
         idResponse = self.get_object(pk)
         if idResponse != 0:
             idResponse = ProfileSerializer(idResponse)
-            print(idResponse.data)
             user = User.objects.filter(id=pk).values()
             responseOk = self.responseCustom(idResponse.data, user, "success", status.HTTP_200_OK)
             return Response(responseOk)
         return Response("Error: No hay datos", status = status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk, format=None):
-        idResponse = self.get_object(pk)
-        if 'image' not in request.data:
-            raise exceptions.ParseError("Ningun archivo seleccionado")
-        file = request.data['image']
-        nombre, formato = os.path.splitext(file.name)
-        urlFinal = ''.join(nombre+formato)
-        urlFinal = 'http://localhost:8000/assets/img_profile/' + urlFinal
-        request.data['url_img'] = urlFinal
-        serializer = ProfileSerializer(idResponse, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            datas = serializer.data
-            responseOk = self.responseCustom(idResponse.data, user, "success", status.HTTP_200_OK)
-            return Response(responseOk)
-        return Response("error", status = status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk, format=None):
+        data = request.data
+        if data is not None:
+            user = User.objects.filter(pk = pk)
+            user.update(username = data.get('username'))
+            user.update(first_name = data.get('first_name'))
+            user.update(last_name = data.get('last_name'))
+            user.update(email = data.get('email'))
+            return Response("Actualizcion exitosa", status.HTTP_200_OK)
+        return Response("Error inesperado", status = status.HTTP_400_BAD_REQUEST)
+    
     def responseCustom(self, data, user, respuesta, status):
         data = self.responseUser(data, user)
         responseOk = {"messages": respuesta,
@@ -85,13 +97,13 @@ class LoadProfileDetail(APIView):
         responsOk = json.dumps(responseOk)
         return responseOk
 
-    def responseUser(self, data, user):
+    def responseUser(self, data, usuario):
         response = {
             "user": data.get('user'),
             "url_img": data.get('url_img'),
-            "username": user[0]['username'],
-            "first_name": user[0]['first_name'],
-            "last_name": user[0]['last_name'],
-            "email": user[0]['email'],
+            "username": usuario[0]['username'],
+            "first_name": usuario[0]['first_name'],
+            "last_name": usuario[0]['last_name'],
+            "email": usuario[0]['email'],
         }
         return response
