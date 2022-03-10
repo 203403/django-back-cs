a9d5b0cfc1ced56b221c70610f103d01ac727c88
Francisco Omar Franco Espinosa <203403@ids.upchiapas.edu.mx>
2022-03-10 10:22:26 -0600

    change the name of call to views

--- Profile/urls.py
+++ Profile/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import include
 # Importacion de vistas
 from Profile.views import LoadProfileImage
 from Profile.views import ProfileUserData
-from Profile.views import CreateNewProfile
 
 urlpatterns = [
-    re_path(r'^user/$', CreateNewProfile.as_view()),
+    re_path(r'^user/$', LoadProfileImage.as_view()),
     re_path(r'^user/(?P<pk>\d+)$', ProfileUserData.as_view()),
     re_path(r'^user-img/(?P<pk>\d+)$', LoadProfileImage.as_view()),
 ]
